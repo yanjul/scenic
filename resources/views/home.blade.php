dced6dc2a989c8223f32f7084dfdbd3881ebecba
moxhotpot <120237937@qq.com>
2017-02-14 14:53:16 +0800

    修改了index.css 和 home.blade.php和layout.css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,13 +9,13 @@
     <div class="index_search clearfix">
         <div class="logo">
             <img src="images/logo.png" style="width: 100px;"><br>
-            <p >FootPrint脚印</p>
+            <p>FootPrint脚印</p>
         </div>
         <div class="search_box">
             <form action="">
                 <div class="form-group">
                     <label for="search"></label>
-                    <input type="text"  id="search" placeholder="请输入景区名称">
+                    <input type="text" id="search" placeholder="请输入景区名称">
                     <a href="#"><span class="glyphicon glyphicon-search"></span></a>
                 </div>
 
@@ -52,47 +52,49 @@
     </div>
 
     <!--滚动图-->
-    <div class="top_content">
-        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-            <!-- Indicators -->
-            <ol class="carousel-indicators">
-                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-            </ol>
+    <div class="bck">
+        <div class="top_content">
+            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                </ol>
 
-            <!-- Wrapper for slides -->
-            <div class="carousel-inner" role="listbox">
-                <div class="item active">
-                    <img src="images/img1.jpg" alt="...">
-                    <div class="carousel-caption">
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" role="listbox">
+                    <div class="item active">
+                        <img src="images/img1.jpg" alt="...">
+                        <div class="carousel-caption">
 
+                        </div>
                     </div>
-                </div>
-                <div class="item">
-                    <img src="images/img2.jpg" alt="...">
-                    <div class="carousel-caption">
+                    <div class="item">
+                        <img src="images/img2.jpg" alt="...">
+                        <div class="carousel-caption">
 
+                        </div>
                     </div>
-                </div>
-                <div class="item">
-                    <img src="images/img3.jpeg" alt="...">
-                    <div class="carousel-caption">
+                    <div class="item">
+                        <img src="images/img3.jpeg" alt="...">
+                        <div class="carousel-caption">
 
+                        </div>
                     </div>
+
                 </div>
 
+                <!-- Controls -->
+                <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
             </div>
-
-            <!-- Controls -->
-            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
         </div>
     </div>
 
