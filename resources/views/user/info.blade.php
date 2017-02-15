7b7d3708f8d2e42409dc837c5118214ce6a89c80
Hgq1001 <1049177554@qq.com>
2017-02-16 00:02:07 +0800

    个人信息页面 景区展示 修改一部分

--- resources/views/user/info.blade.php
+++ resources/views/user/info.blade.php
@@ -4,7 +4,69 @@
     <div class="container main">
         <div class="row">
             @include('user.menu')
-            <div class="col-md-9 right-main" style="border: 1px solid;">
+            <div class="col-md-9">
+                <div class="breadcrumb">
+                    <a href="/user">我的脚印 </a>
+                    >
+                    <span>个人信息</span>
+                </div>
+                <div class="tab-myfoot">
+                    <ul class="title">
+                        <li>
+                            <a href="" class="on">基本资料</a>
+                        </li>
+                        <li>
+                            <a href="">头像设置</a>
+                        </li>
+                        <li>
+                            <a href="">兴趣爱好</a>
+                        </li>
+                    </ul>
+                    <div class="c-n box01">
+                        <div class="per-edit-list">
+                            <form action="" id="myinfoform">
+                                <div class="per-edit-msg">完善更多个人信息，有助于我们为您提供更加个性化的服务，脚印将尊重并保护您的隐私。</div>
+                                <dl>
+                                    <dt>昵&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;称：</dt>
+                                    <dd>
+                                        <input type="text" class="input-text-2">
+                                        <span class="onSuccess">{{$info['name']}}</span>
+                                    </dd>
+                                </dl>
+                                <dl>
+                                    <dt>真实姓名：</dt>
+                                    <dd><input type="text" class="input-text-2"></dd>
+                                </dl>
+                                <dl>
+                                    <dt>公司地址：</dt>
+                                    <dd>
+                                        <input type="text" id="company_adress" class="input-text-3">
+                                    </dd>
+                                </dl>
+                                <dl>
+                                    <dt>邮&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;箱：</dt>
+                                    <dd>
+                                        <input type="text" id="email" class="input-text-3">
+                                    </dd>
+                                </dl>
+                                <dl>
+                                    <dt>传&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;真：</dt>
+                                    <dd>
+                                        <input type="text" id="fax" class="input-text-3">
+                                    </dd>
+                                </dl>
+                                <dl>
+                                    <dt>备&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;注：</dt>
+                                    <dd>
+                                        <textarea  id="remark" class="input-text-3" style="resize: none;width: 300px;height: 80px;min-height: 60px;min-width: 200px;"></textarea>
+                                    </dd>
+                                </dl>
+                                <button id="sumBgn" class="btn05" type="submit">保存</button>
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
                 <div>用户名: {{$info['name']}}</div>
                 <div>头像
                     @if($info['info']['photo'])
--- resources/views/user/scenic.blade.php
+++ resources/views/user/scenic.blade.php
@@ -11,22 +11,37 @@
                     <span>所有景区</span>
                 </div>
                 @foreach($list as $item)
-                    <div style="border: 2px solid #f0ad4e; display: inline-block; vertical-align: top; width: 240px; height: 240px">
-                        <p>{{$item['name']}}</p>
-                        <p>
-                            <img src="{{$item['image']}}" width="220px">
-                        </p>
-                        <p>{{$item['info']}}</p>
-                        <p>
-                            <a href="{{url('user/add-scenic/'.$item['id'])}}">修改</a>
-                            <a href="{{url('user/del-scenic/'.$item['id'])}}">删除</a>
-                        </p>
-                        <p>
-                            <a href="{{url('user/scenic/'.$item['id'])}}">查看门票</a>
-                            <a href="{{url('user/add-ticket/'.$item['id'])}}">添加门票</a>
-                        </p>
+                <div class="scenic-list">
+                    <div class="media">
+                        <div class="media-left">
+                            <a href="#">
+                                <img src="public/images/scenic/wulong1.jpg" alt="" class="media-object">
+                            </a>
+                        </div>
+                        <div class="media-body">
+                            <h4 class="media-heading">{{$item['name']}}</h4>
+                            <p>{{$item['info']}}</p>
+                        </div>
                     </div>
+                </div>
                 @endforeach
+                {{--@foreach($list as $item)--}}
+                    {{--<div style="border: 2px solid #f0ad4e; display: inline-block; vertical-align: top; width: 240px; height: 240px">--}}
+                        {{--<p>{{$item['name']}}</p>--}}
+                        {{--<p>--}}
+                            {{--<img src="{{$item['image']}}" width="220px">--}}
+                        {{--</p>--}}
+                        {{--<p>{{$item['info']}}</p>--}}
+                        {{--<p>--}}
+                            {{--<a href="{{url('user/add-scenic/'.$item['id'])}}">修改</a>--}}
+                            {{--<a href="{{url('user/del-scenic/'.$item['id'])}}">删除</a>--}}
+                        {{--</p>--}}
+                        {{--<p>--}}
+                            {{--<a href="{{url('user/scenic/'.$item['id'])}}">查看门票</a>--}}
+                            {{--<a href="{{url('user/add-ticket/'.$item['id'])}}">添加门票</a>--}}
+                        {{--</p>--}}
+                    {{--</div>--}}
+                {{--@endforeach--}}
             </div>
         </div>
     </div>
