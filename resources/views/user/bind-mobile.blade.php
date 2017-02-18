8cd8ed29b4dd2358963a200aaf9feb872732b79c
Hgq1001 <1049177554@qq.com>
2017-02-18 17:51:54 +0800

    hgq

--- resources/views/user/bind-mobile.blade.php
+++ resources/views/user/bind-mobile.blade.php
@@ -5,26 +5,56 @@
         <div class="row">
             @include('user.menu')
             <div class="col-md-9">
+                <div class="breadcrumb">
+                    <a href="/user">我的脚印</a>
+                    >
+                    <span>绑定手机</span>
+                </div>
                 @if(Auth::user()->telephone)
-                    <span>您已绑定手机</span>
+                <span>您已绑定手机</span>
                 @else
-                    <form id="code-form" class="form-horizontal" action="/user/bind-mobile" method="post">
-                        {{ csrf_field() }}
-                        <input id="msg-id" type="hidden" name="msg_id">
-                        <div class="form-group">
-                            <label class="control-label" for="mobile">手机号</label>
-                            <input class="form-control" type="tel" id="mobile" name="mobile" value="{{old('mobile')}}">
-                            @if($errors->has('mobile'))
-                                <span>格式错误</span>
-                            @endif
-                        </div>
-                        <div class="form-group">
-                            <label for="verification-code">验证码</label>
-                            <input class="form-control" type="tel" id="verification-code" name="code" value="{{old('code')}}">
-                            <button id="btn-code" type="button">获取验证码</button>
-                        </div>
-                        <button class="btn" type="submit">绑定</button>
-                    </form>
+                    <div class="section">
+                        <ul class="popup-address">
+                            <li>
+                                <p class="caption01">
+                                    <span class="c4">*&nbsp;</span>
+                                    手机号：
+                                </p>
+                                <div class="info">
+                                    <input type="tel" class="t" id="mobile" name="mobile" value="{{old('mobile')}}">
+                                    @if($errors->has('mobile'))
+                                        <span>格式错误</span>
+                                    @endif
+                                </div>
+                            </li>
+                            <li>
+                                <p class="caption01">
+                                    <span class="c4">*&nbsp;</span>
+                                    验证码：
+                                </p>
+                                <div class="info">
+                                    <input type="tel" class="t" id="verification-code" name="code" value="{{old('code')}}">
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                <form id="code-form" class="form-horizontal" action="/user/bind-mobile" method="post">
+                    {{ csrf_field() }}
+                    <input id="msg-id" type="hidden" name="msg_id">
+                    <div class="form-group">
+                        <label class="control-label" for="mobile">手机号</label>
+                        <input class="form-control" type="tel" id="mobile" name="mobile" value="{{old('mobile')}}">
+                        @if($errors->has('mobile'))
+                            <span>格式错误</span>
+                        @endif
+                    </div>
+                    <div class="form-group">
+                        <label for="verification-code">验证码</label>
+                        <input class="form-control" type="tel" id="verification-code" name="code" value="{{old('code')}}">
+                        <button id="btn-code" type="button">获取验证码</button>
+                    </div>
+                    <button class="btn" type="submit">绑定</button>
+                </form>
                     <script>
                         window.onload = function () {
                             $('#btn-code').click(function () {
