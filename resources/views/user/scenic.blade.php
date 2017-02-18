9c3b5e0e999610c7f444023e1758752a319c12f5
Hgq1001 <1049177554@qq.com>
2017-02-18 17:45:44 +0800

    hgq

--- resources/views/user/scenic.blade.php
+++ resources/views/user/scenic.blade.php
@@ -7,32 +7,41 @@
             <div class="col-md-9">
                 <div class="breadcrumb">
                     <a href="/user">我的脚印</a>
-
+                    >
                     <span>所有景区</span>
                 </div>
-                @foreach($list as $item)
-                    <div class="scenic-list">
-                        <div class="media">
-                            <div class="media-left">
-                                <a href="#">
-                                    <img src="{{$item['image']}}" alt="" class="media-object">
-                                </a>
-                            </div>
-                            <div class="media-body">
-                                <h4 class="media-heading">{{$item['name']}}</h4>
-                                <p>{{$item['info']}}</p>
+                @if(count($list))
+                    @foreach($list as $item)
+                        <div class="scenic-list">
+                            <div class="media">
+                                <div class="media-left">
+                                    <a href="#" class="imgBox">
+                                        <img src="{{$item['image']}}" alt="" class="media-object" width="440px" height="260px">
+                                        <p class="imgTitle">{{$item['name']}}</p>
+                                    </a>
+                                </div>
+                                <div class="media-body">
+                                    <h4 class="media-heading"><a href="" class="scenic-title">{{$item['name']}}</a></h4>
+                                    <p class="scenic-content">{{$item['info']}}</p>
+                                    <div class="handle">
+                                        <p class="edit-btn">
+                                            <a href="{{url('user/scenic/'.$item['id'])}}" class="btn btn-primary">查看门票</a>
+                                            <a href="{{url('user/add-ticket/'.$item['id'])}}" class="btn btn-success">添加门票</a>
+                                            <a href="{{url('user/add-scenic/'.$item['id'])}}" class="btn btn-info btn-3">修改景区</a>
+                                            <a href="{{url('user/del-scenic/'.$item['id'])}}" class="btn btn-danger">删除景区</a>
+                                        </p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
+                    @endforeach
+                @else
+                    <div class="scenic-list">
+
                     </div>
-                    <p>
-                        <a href="{{url('user/add-scenic/'.$item['id'])}}">修改</a>
-                        <a href="{{url('user/del-scenic/'.$item['id'])}}">删除</a>
-                    </p>
-                    <p>
-                        <a href="{{url('user/scenic/'.$item['id'])}}">查看门票</a>
-                        <a href="{{url('user/add-ticket/'.$item['id'])}}">添加门票</a>
-                    </p>
-                @endforeach
+
+                @endif
+
                 {{--@foreach($list as $item)--}}
                     {{--<div style="border: 2px solid #f0ad4e; display: inline-block; vertical-align: top; width: 240px; height: 240px">--}}
                         {{--<p>{{$item['name']}}</p>--}}
