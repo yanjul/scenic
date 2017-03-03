d4c05044b52c24f3b063e9afb43917a4188ee59c
Hgq1001 <1049177554@qq.com>
2017-03-03 09:43:17 +0800

    门票展示页面修改

--- resources/views/user/ticket.blade.php
+++ resources/views/user/ticket.blade.php
@@ -13,37 +13,38 @@
                 <div class="section">
                     <div class="con">
                         <ul class="ticket-info">
-                            <li>
-                                <a href="" class="img-box">
-                                    <img src="{{$scenic['image']}}" width="300" height="240" class="scenic-img">
-                                </a>
-                                @foreach($scenic['ticket'] as $ticket)
-                                <div class="ri-infos">
-                                    <h5>景区名字：{{$scenic['name']}}</h5>
-                                    @foreach($ticket['custom_price'] as $item)
-                                    <p>
+                            @foreach($scenic['ticket'] as $ticket)
+                                <li>
+                                    <a href="" class="img-box">
+                                        <img src="{{$scenic['image']}}" width="300" height="240" class="scenic-img">
+                                    </a>
+                                    <div class="ri-infos">
+                                        <h5>景区名字：{{$scenic['name']}}</h5>
+                                        @foreach($ticket['custom_price'] as $item)
+                                            <p>
                                         <span class="start-time">
                                             开始时间：<strong>{{date('Y-m-d H:i:s', $item['start_time'])}}</strong>
                                         </span>
                                         <span class="end-time">
                                             结束时间：<strong>{{date('Y-m-d H:i:s', $item['end_time'])}}</strong>
                                         </span>
-                                    </p>
-                                    @endforeach
-                                    <div class="ticket-price">
-                                        <span>票价：{{$item['price']}}元</span>
+                                            </p>
+                                        @endforeach
+                                        <div class="ticket-price">
+                                            <span>票价：{{$item['price']}}元</span>
+                                        </div>
+                                        <p class="valid-time">有效时间：{{$ticket['valid_time']}}天</p>
+                                        <p class="lead-time">提前时间：{{$ticket['lead_time']}}天</p>
+                                        <p class="latest-time">最迟入园时间：{{$ticket['last_time']}} 点</p>
+                                        <p class="remark">备注：<span class="remark-box">{{$ticket['remark']}}</span></p>
+                                        <div class="edit-btns">
+                                            <a href="{{url('user/scenic/ticket/'.$ticket['id'])}}" class="update-btn">修改门票</a>
+                                            <a href="{{url('user/del-ticket/'.$ticket['id'])}}"
+                                               class="delete-btn">删除门票</a>
+                                        </div>
                                     </div>
-                                    <p class="valid-time">有效时间：{{$ticket['valid_time']}}天</p>
-                                    <p class="lead-time">提前时间：{{$ticket['lead_time']}}天</p>
-                                    <p class="latest-time">最迟入园时间：{{$ticket['last_time']}} 点</p>
-                                    <p class="remark">备注：<span class="remark-box">{{$ticket['remark']}}</span></p>
-                                    <div class="edit-btns">
-                                        <a href="{{url('user/scenic/ticket/'.$ticket['id'])}}" class="update-btn">修改门票</a>
-                                        <a href="{{url('user/del-ticket/'.$ticket['id'])}}" class="delete-btn">删除门票</a>
-                                    </div>
-                                </div>
-                                @endforeach
-                            </li>
+                                </li>
+                            @endforeach
                         </ul>
                     </div>
                 </div>
