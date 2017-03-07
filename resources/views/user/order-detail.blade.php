c7d2f300a65c89781503ea7534cff7cdbcb400fd
Hgq1001 <1049177554@qq.com>
2017-03-07 22:55:08 +0800

    订单页面

---
+++ resources/views/user/order-detail.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            @include('user.menu')
+            <div class="col-md-9">
+                <div class="breadcrumb">
+                    <a href="/user">我的脚印</a>
+                    >
+                    <span>订单详情</span>
+                </div>
+                div
+            </div>
+        </div>
+    </div>
+@endsection
