3c69fa90c450f8670d352b06637fd058eb64d61b
moxhotpot <120237937@qq.com>
2017-03-07 14:57:18 +0800

    登录注册

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,7 +14,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">登录x</div>
+                <div class="panel-heading">登录</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
