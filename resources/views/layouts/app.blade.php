16061ff7a64bcd55aaf6261a86fb34e4adc3bdef
Yan Jun <yanjun@yilu-tech.com>
2017-03-15 14:07:34 +0800

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,16 +4,14 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{{ config('app.name', 'scenic') }}</title>
-
     <!-- Styles -->
+    <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="/css/layout.css">
-@yield('css')
+    @yield('css')
 <!-- Scripts -->
     <script>
         window.Laravel = {!! json_encode([ 'csrfToken' => csrf_token() ]) !!};
@@ -53,10 +51,9 @@
         @yield('content')
     </div>
     <!-- Scripts -->
-    <script src="/js/jQuery.js"></script>
-    <script src="/js/bootstrap.js"></script>
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+    <script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <script src="/js/select.js"></script>
-
     @yield('js')
 </body>
 </html>
